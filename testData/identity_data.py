7d13d90f8d6c8f4128a8a335d5b43ee5684da626
bezobrazieq <you@example.com>
2022-07-31 23:44:40 +0300

    fix tests, use dataclasses

--- testData/identity_data.py
+++ testData/identity_data.py
@@ -1,5 +1,6 @@
 from testData import TestContext
 from testData.models.view_models import RegisterVM, LoginVM
+from testData.models.parametrize_models import ParametrizeModel
 
 
 class IdentityData:
@@ -56,50 +57,48 @@ class IdentityData:
                 "lastName": "Ельцын",
                 "patronymic": None,
                 "phoneNumber": "+79657020827"})
-    })
+        })
 
-    # Данные используемые в тестах по авторизации с некорректным логином.
-    BAD_LOGIN = LoginVM.from_dict({
-        "email": "post@mail1.ru",
-        "password": "Test123456"
-    })
-
-    # Данные используемые в тестах по авторизации с некорректным паролем.
-    BAD_PASSWORD = LoginVM.from_dict({
-        "email": "post@mail.ru",
-        "password": "Test12345"
-    })
-
-    # Данные используемые в тестах по успешной авторизации.
-    VALID_AUTORIZATION = TestContext.from_dict({
-        "email": "post@mail.ru",
-        "password": "Test123456"
+    # Данные для авторизации валидные/невалидные
+    AUTORIZATION_DATA = TestContext.from_dict({
+        "valid_data": LoginVM.from_dict({
+            "email": "post@mail.ru",
+            "password": "Test123456"}),
+        "bad_login": LoginVM.from_dict({
+            "email": "post@mail1.ru",
+            "password": "Test123456"}),
+        "bad_password": LoginVM.from_dict({
+            "email": "post@mail.ru",
+            "password": "Test12345"})
     })
 
     # Данные используемые в параметризации для негативных тестов регистрации
     DATA_FOR_BAD_REG = [
         TestContext.from_dict({
-         "input": INVALID_REGISTRATION_DATA.get("bad_confirm_password"),
-         "expected": "Пароли не совпадают",
-         "case_name": "Registering with mismatched passwords"}),
+            "case": ParametrizeModel.from_dict({
+                "input": INVALID_REGISTRATION_DATA.get("bad_confirm_password"),
+                "expected": "Пароли не совпадают",
+                "case_name": "Registering with mismatched passwords"})}),
         TestContext.from_dict({
-         "input": INVALID_REGISTRATION_DATA.get("bad_email"),
-         "expected": "Электронный адрес должен соответствовать регулярному выражению ^[-\\w.]+@([A-z0-9][-A-z0-9]+\\.)+[A-z]{2,}$",
-         "case_name": "Email does not pass regular expression"}),
+            "case": ParametrizeModel.from_dict({
+                "input": INVALID_REGISTRATION_DATA.get("bad_email"),
+                "expected": "Электронный адрес должен соответствовать регулярному выражению ^[-\\w.]+@([A-z0-9][-A-z0-9]+\\.)+[A-z]{2,}$",
+                "case_name": "Email does not pass regular expression"})}),
         TestContext.from_dict({
-          "input": INVALID_REGISTRATION_DATA.get("bad_phone"),
-          "expected": "Номер телефона должен соответствовать регулярному выражению ^\\+7\\d{10}$",
-          "case_name": "Phone number doesn't pass regular expression"})
-    ]
+            "case": ParametrizeModel.from_dict({
+                "input": INVALID_REGISTRATION_DATA.get("bad_phone"),
+                "expected": "Номер телефона должен соответствовать регулярному выражению ^\\+7\\d{10}$",
+                "case_name": "Phone number doesn't pass regular expression"})})]
 
     # Данные используемые в параметризации для негативных тестов авторизации, обернуты в контекст
     DATA_FOR_BAD_AUTH = [
         TestContext.from_dict({
-         "input": BAD_LOGIN,
-         "expected": f"Пользователь с email {BAD_LOGIN.email} не найден",
-         "case_name": "Login with incorrect login"}),
+            'case': ParametrizeModel.from_dict({
+                "input": AUTORIZATION_DATA.get("bad_login"),
+                "expected": f"Пользователь с email {AUTORIZATION_DATA.get('bad_login').email} не найден",
+                "case_name": "Login with incorrect login"})}),
         TestContext.from_dict({
-         "input": BAD_PASSWORD,
-         "expected": "Неверный пароль",
-         "case_name": "Login with incorrect password"})
-    ]
+            'case': ParametrizeModel.from_dict({
+                "input": AUTORIZATION_DATA.get("bad_password"),
+                "expected": "Неверный пароль",
+                "case_name": "Login with incorrect password"})})]
