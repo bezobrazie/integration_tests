7a8964dd3f5b8b64589e1359046d89175ef4e433
bezobrazieq <you@example.com>
2022-08-22 10:15:15 +0300

    added new methods for account tests
    added new methods for account tests
    + new model Account
    + new fixtures: clean_data_base_before_scenario, db_client, db_cleaner, db_seeder

--- tests/test_create_account/conftest.py
+++ tests/test_create_account/conftest.py
@@ -3,37 +3,38 @@ import pytest
 from appDriver.dbDriver.db_seeder import DBSeeder
 from appDriver.dbDriver.db_cleaner import DBCleaner
 from testData.scenarios.create_account_scenario import INITIAL_DATA
-from psycopg2.errors import UniqueViolation, InFailedSqlTransaction
 
 
-# TODO: продумать обработку ошибок при попытке добавления записи, которая уже добавлена
+@pytest.fixture(scope='session', autouse=True)
+def clean_data_base_before_scenario(db_cleaner: DBCleaner):
+    """
+    Фикстура для очистки базы данных до начала тестирования
+    :param db_cleaner: экземпляр класса  DBCleaner, инициализируется в фикстуре в общем конфтесте.
+    """
+    tables = ['\"AspNetUsers\"']
+    db_cleaner.clean_tables(tables)
+
+
 @pytest.fixture(scope='session')
-def seed_users_before_scenario(db_connection):
+def seed_users_before_scenario(db_cleaner: DBCleaner, db_seeder: DBSeeder):
     """
     Фикстура для заполнения таблиц перед тестами, и чистки после тестов
-    :param db_connection: psycopg2.connect объект коннекта к базе данных, нужен для инициализации классов для работы с БД
+    :param db_cleaner: экземпляр класса  DBCleaner, инициализируется в фикстуре в общем конфтесте.
+    :param db_seeder: экземпляр класса  DBSeeder, инициализируется в фикстуре в общем конфтесте.
     """
-    db_cleaner = DBCleaner(db_connection)
-    db_seeder = DBSeeder(db_connection)
-    try:
-        db_seeder.seed_users(INITIAL_DATA['users'])
-    except (InFailedSqlTransaction, UniqueViolation):
-        tables = ['\"AspNetUsers\"']
-        db_cleaner.clean_tables(tables)
-        db_seeder.seed_users(INITIAL_DATA['users'])
+    db_seeder.seed_users(INITIAL_DATA['users'])
     yield
     tables = ['\"AspNetUsers\"']
     db_cleaner.clean_tables(tables)
 
 
 @pytest.fixture(scope='session')
-def seed_addresses_before_scenario(db_connection):
+def seed_addresses_before_scenario(db_cleaner: DBCleaner, db_seeder: DBSeeder):
     """
     Фикстура для заполнения таблицы адресс перед тестами, и чистки после тестов
-    :param db_connection: psycopg2.connect объект коннекта к базе данных, нужен для инициализации классов для работы с БД
+    :param db_cleaner: экземпляр класса  DBCleaner, инициализируется в фикстуре в общем конфтесте.
+    :param db_seeder: экземпляр класса  DBSeeder, инициализируется в фикстуре в общем конфтесте.
     """
-    db_cleaner = DBCleaner(db_connection)
-    db_seeder = DBSeeder(db_connection)
     db_seeder.seed_addresses(INITIAL_DATA['addresses'])
     yield
     tables = ["addresses"]
@@ -41,13 +42,12 @@ def seed_addresses_before_scenario(db_connection):
 
 
 @pytest.fixture(scope='session')
-def seed_passports_before_scenario(db_connection):
+def seed_passports_before_scenario(db_cleaner: DBCleaner, db_seeder: DBSeeder):
     """
     Фикстура для заполнения таблицы паспорт перед тестами, и чистки после тестов
-    :param db_connection: psycopg2.connect объект коннекта к базе данных, нужен для инициализации классов для работы с БД
+    :param db_cleaner: экземпляр класса  DBCleaner, инициализируется в фикстуре в общем конфтесте.
+    :param db_seeder: экземпляр класса  DBSeeder, инициализируется в фикстуре в общем конфтесте.
     """
-    db_cleaner = DBCleaner(db_connection)
-    db_seeder = DBSeeder(db_connection)
     db_seeder.seed_passport(INITIAL_DATA['passports'])
     yield
     tables = ["passports"]
